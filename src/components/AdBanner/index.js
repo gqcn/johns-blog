5c5b9c51e65804d8692226a8460825f756e9dda7
John <john@johng.cn>
2025-04-08 19:24:16 +0800

    doc update

--- src/components/AdBanner/index.js
+++ src/components/AdBanner/index.js
@@ -5,8 +5,9 @@ export default function AdBanner() {
   return (
     <div className={styles.adContainer}>
       <div className={styles.adBanner}>
-        {/* 广告内容将在这里显示 */}
-        <p className={styles.adText}>广告位 - 您可以在这里放置您的广告内容</p>
+        {/* 万维广告 */}
+        <div className="wwads-cn wwads-horizontal" data-id="350"></div>
+        {/* <p className={styles.adText}>广告位 - 您可以在这里放置您的广告内容</p> */}
       </div>
     </div>
   );
